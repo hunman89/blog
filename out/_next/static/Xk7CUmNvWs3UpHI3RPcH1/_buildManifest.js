fcae94a4a80e5a54072c3d8754cd7dce1d30f242
hunman89 <hunman89@gmail.com>
2022-12-18 17:44:42 +0900

    add. index page

--- out/_next/static/Xk7CUmNvWs3UpHI3RPcH1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4bc857efe4096191.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/about":["static/chunks/pages/about-37c3b771c8309532.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
