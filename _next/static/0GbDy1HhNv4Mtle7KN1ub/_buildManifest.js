0f036d5e2ca780ad6e9b44636f2fd51e349331da
jungsunghun <sunghun_jung@tmax.co.kr>
2022-12-19 17:15:04 +0900

    add. sub menu

---
+++ _next/static/0GbDy1HhNv4Mtle7KN1ub/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8dd0d44eaf4fd3e2.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/about":["static/chunks/pages/about-37c3b771c8309532.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
